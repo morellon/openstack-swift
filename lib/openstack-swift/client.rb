d7f7147a31885168ce2a99cf3ac7a4c74eaaed1c
PotHix <pothix@pothix.com>
2011-08-15 16:11:29 -0300

    Adding some Docs and a new test

--- lib/openstack-swift/client.rb
+++ lib/openstack-swift/client.rb
@@ -1,11 +1,12 @@
 module Openstack
   module Swift
     class Client
+      # Authentication method
+      # It stores the authentication url and token for future commands
+      # Avoiding to request a new token for each request
       def authenticate(proxy, user, password)
         @url, _, @token = Openstack::Swift::WebApi.auth(proxy, user, password)
 
-        puts @url
-        puts @token
         if @url.blank? or @token.blank?
           raise AuthenticationError
         else
@@ -13,12 +14,22 @@ module Openstack
         end
       end
 
+      # Returns the following informations about the account:
+      #   bytes_used: Number of bytes used by this account
+      #   object_count: Number of objects that this account have allocated
+      #   container_count: Number of container
+      def account_info
+        Openstack::Swift::WebApi.account(@url, @token)
+      end
+
+      # This method uploads a file to a given container
       def upload(container, file_path)
         Openstack::Swift::WebApi.upload_object(@url, @token, container, file_path)
       end
 
-      def download(container, file_path)
-        Openstack::Swift::WebApi.download_object(@url, @token, container, file_path)
+      # This method downloads a object from a given container
+      def download(container, object)
+        Openstack::Swift::WebApi.download_object(@url, @token, container, object)
       end
     end
   end
--- spec/openstack-swift/client_spec.rb
+++ spec/openstack-swift/client_spec.rb
@@ -1,11 +1,22 @@
 require "openstack-swift"
 
+URL = "https://sw:8080/auth/v1.0"
+USER = "system:root"
+PASS = "testpass"
+
 describe Openstack::Swift::Client do
   context "when authenticating" do
     it "should return authentication error if one of the parameters is incorrect" do
       expect {
         subject.authenticate("http://incorrect.com/swift", USER, PASS)
       }.to raise_error(Openstack::Swift::AuthenticationError)
+    end
+
+    it "shoud not need to authenticate again" do
+      expect {
+        subject.authenticate(URL, USER, PASS)
+        subject.account_info
+      }.to_not raise_error
     end
   end
 end
