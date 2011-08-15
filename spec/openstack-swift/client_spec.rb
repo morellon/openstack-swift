498f112796285126a7f9d503fe8f1c4bf72adb4f
PotHix <pothix@pothix.com>
2011-08-15 15:34:28 -0300

    Removing double constant definitions

--- spec/openstack-swift/client_spec.rb
+++ spec/openstack-swift/client_spec.rb
@@ -1,9 +1,5 @@
 require "openstack-swift"
 
-URL = "https://sw:8080/auth/v1.0"
-USER = "system:root"
-PASS = "testpass"
-
 describe Openstack::Swift::Client do
   context "when authenticating" do
     it "should return authentication error if one of the parameters is incorrect" do
