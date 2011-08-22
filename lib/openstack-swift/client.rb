1a84a2ee8ea598f27975dbd3f14296a4d58c326e
PotHix <pothix@pothix.com>
2011-08-22 11:37:07 -0300

    Refactoring the upload method and adding some documentation

--- lib/openstack-swift/client.rb
+++ lib/openstack-swift/client.rb
@@ -4,14 +4,17 @@ module Openstack
     class Client
       MAX_SIZE = 4 * 1024 ** 3
 
-      # initialize method
-      # It stores the authentication url and token for future commands
-      # Avoiding to request a new token for each request
+      # Initialize method
+      # It uses the authenticate method to store the tokens for future requests
       def initialize(proxy, user, password)
         @proxy, @user, @password = proxy, user, password
         authenticate!
       end
 
+      # Authentication method
+      # It stores the authentication url and token for future commands
+      # avoiding to request a new token for each request
+      # It should be used to force a new token
       def authenticate!
         @url, _, @token = Openstack::Swift::WebApi.auth(@proxy, @user, @password)
 
@@ -52,7 +55,7 @@ module Openstack
         }
       end
 
-      # This method uploads a file to a given container
+      # This method uploads a file from a given to a given container
       def upload(container, file_path, options={})
         options[:segments_size] ||= MAX_SIZE
 
@@ -69,11 +72,23 @@ module Openstack
           last_piece = File.size(file_path) - segments_minus_one * options[:segments_size]
           segments_minus_one.times do |i|
             segment_path = "#{file_name}/#{file_mtime}/#{file_size}/%08d" % i
-            Openstack::Swift::WebApi.upload_object(@url, @token, "#{container}_segments", file_path, options[:segments_size] * i, options[:segments_size], segment_path)
+            Openstack::Swift::WebApi.upload_object(
+              @url, @token, "#{container}_segments", file_path,
+              :size => options[:segments_size],
+              :position => options[:segments_size] * i,
+              :object_name => segment_path
+            )
           end
 
           segment_path = "#{file_name}/#{file_mtime}/#{file_size}/%08d" % segments_minus_one
-          Openstack::Swift::WebApi.upload_object(@url, @token, "#{container}_segments", file_path, options[:segments_size] * segments_minus_one, last_piece, segment_path)
+
+          Openstack::Swift::WebApi.upload_object(
+            @url, @token, "#{container}_segments", file_path,
+            :size => last_piece,
+            :position => options[:segments_size] * segments_minus_one,
+            :object_name => segment_path
+          )
+
           Openstack::Swift::WebApi.create_manifest(@url, @token, container, file_path)
         else
           Openstack::Swift::WebApi.upload_object(@url, @token, container, file_path)
--- lib/openstack-swift/web_api.rb
+++ lib/openstack-swift/web_api.rb
@@ -4,6 +4,11 @@ module Openstack
     module WebApi
       extend self
 
+      # Authentication method to get the url and token to conect to swift
+      # Returns:
+      #   x-storage-url
+      #   x-storage-token
+      #   x-auth-token
       def auth(proxy, user, password)
         res = HTTParty.get(proxy, :headers => {"X-Auth-User" => user, "X-Auth-Key" => password})
         raise AuthenticationError unless res.code == 200
@@ -11,6 +16,11 @@ module Openstack
         [res.headers["x-storage-url"],res.headers["x-storage-token"],res.headers["x-auth-token"]]
       end
 
+      # Get informations about the currect account used to connect to swift
+      # Returns:
+      #   x-account-bytes-used
+      #   x-account-object-count
+      #   x-account-container-count
       def account(url, token)
         query = {:format => "json"}
         HTTParty.head(url, :headers => {"X-Auth-Token"=> token}, :query => query).headers
@@ -27,31 +37,42 @@ module Openstack
         res.to_a
       end
 
-      # query options: marker, prefix, limit, delimiter
+      # Get all objects for a given container
+      # Query options:
+      #   marker
+      #   prefix
+      #   limit
+      #   delimiter
       def objects(url, token, container, query = {})
         query = query.merge(:format => "json")
         res = HTTParty.get("#{url}/#{container}", :headers => {"X-Auth-Token"=> token}, :query => query)
         res.to_a
       end
 
+      # Delete a container for a given name from swift
       def delete_container(url, token, container)
         res = HTTParty.delete("#{url}/#{container}", :headers => {"X-Auth-Token"=> token})
         raise "Could not delete container '#{container}'" if res.code < 200 or res.code >= 300
         true
       end
 
+      # Create a container on swift from a given name
       def create_container(url, token, container)
         res = HTTParty.put("#{url}/#{container}", :headers => {"X-Auth-Token"=> token})
         raise "Could not create container '#{container}'" if res.code < 200 or res.code >= 300
         true
       end
 
+      # Get the object stat given the object name and the container this object is in
       def object_stat(url, token, container, object)
         url = "#{url}/#{container}/#{object}"
         query = {:format => "json"}
         HTTParty.head(url, :headers => {"X-Auth-Token"=> token}, :query => query).headers
       end
 
+      # Creates the manifest file for a splitted upload
+      # Given the container and file path a manifest is created to guide the downloads of this
+      # splitted file
       def create_manifest(url, token, container, file_path)
         file_name = file_path.match(/.+\/(.+?)$/)[1]
         file_size  = File.size(file_path)
@@ -101,17 +122,17 @@ module Openstack
       end
 
       # Uploads a given object to a given container
-      def upload_object(url, token, container, file_path, position = nil, size = nil, object_name=nil)
-        object_name ||= file_path.match(/.+\/(.+?)$/)[1]
+      def upload_object(url, token, container, file_path, options={})
+        options[:object_name] ||= file_path.match(/.+\/(.+?)$/)[1]
         file = File.open(file_path, "rb")
 
-        file.seek(position) if position
-        uri = URI.parse("#{url}/#{container}/#{object_name}")
+        file.seek(options[:position]) if options[:position]
+        uri = URI.parse("#{url}/#{container}/#{options[:object_name]}")
 
         req = Net::HTTP::Put.new(uri.path)
         req.add_field("X-Auth-Token", token)
         req.body_stream = file
-        req.content_length = size || File.size(file_path)
+        req.content_length = options[:size] || File.size(file_path)
         req.content_type = "application/octet-stream"
 
         http = Net::HTTP.new(uri.host, uri.port)
